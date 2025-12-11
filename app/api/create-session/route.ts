76a1a8b3e73000ef32191c92522395429addfa71
sksk0301 <sksk0301@bwc.co.kr>
2025-12-12 05:24:23 +0900

    Set runtime to 'edge' for create-session route

--- app/api/create-session/route.ts
+++ app/api/create-session/route.ts
@@ -1,5 +1,6 @@
 import { WORKFLOW_ID } from "@/lib/config";
 import { config as dotenvLoad } from "dotenv";
+export const runtime = 'edge';
 
 if (process.env.NODE_ENV !== "production") {
   // Load .env.local data over preset env variables
